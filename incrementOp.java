224d9e4c8e7a331fb67bc7c3036a3ae23b8f7137
Rohan Veer <49567271+RohanUttamVeer@users.noreply.github.com>
2021-02-27 11:46:04 +0530

    increment operator logic

---
+++ incrementOp.java
@@ -0,0 +1,13 @@
+class incrementOp {
+    public static void main(String[] args) {
+        int var1 = 5, var2 = 5;
+
+        // var1 is displayed
+        // Then, var1 is increased to 6.
+        System.out.println(var1++);
+
+        // var2 is increased to 6
+        // Then, var2 is displayed
+        System.out.println(++var2);
+    }
+}
