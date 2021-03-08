874ab874b8aa286a7dc0bcef798def8e464620f7
Rohan Veer <49567271+RohanUttamVeer@users.noreply.github.com>
2021-03-08 15:27:16 +0530

    for loop and its syntax

---
+++ forLoop.java
@@ -0,0 +1,9 @@
+class forLoop{
+	public static void main(String[] args){ /*String[] args or String args[] both are similar but most recommended is String[] args
+	                                         *it creates string array called args
+														  */
+		for( int i = 1; i <= 10; ++i){ //syntax : for(initialExpression; testExpression; updateExpression){}
+			System.out.println(i); //prints integers from 1 to 10
+		}
+	}
+}
