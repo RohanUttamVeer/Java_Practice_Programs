a99c81c3b99ea48ff1743f39d4b4fa281cb7e49a
Rohan Veer <49567271+RohanUttamVeer@users.noreply.github.com>
2021-03-26 14:15:55 +0530

    sum of first 10 natural numbers

---
+++ for2.java
@@ -0,0 +1,10 @@
+//Write a program to calculate the sum of first 10 natural number.
+class for2{
+	public static void main(String args[]){
+	int result = 0;
+		for(int i = 1; i <=10; i++){
+		  result += i;
+		}
+		System.out.println(result);
+	}
+}
